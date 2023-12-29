a8f637674f15b945cf1b5bd9efbd286370e3a319
SurajTechsmith <surajyadav27092001@gmail.com>
2023-12-29 15:42:13 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+                 <div className='section one'>
+        <h1>HELLO</h1>
+      </div>
+      <div className='section second'>
+        <h1>World</h1>
+      </div> 
+      <div className='section third'>
+  
+        <video  autoPlay muted loop>
+          <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4" type="video/mp4" />
+        </video>
+   
+      </div>
+  
     </div>
   );
 }
